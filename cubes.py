2a721cd2a04151261ed4887da66096ff1397d414
Tony-Otis <otieno.anthonyomondi00@gmail.com>
2021-07-16 11:51:49 +0300

    Update cubes.py

--- cubes.py
+++ cubes.py
@@ -4,6 +4,6 @@ x_values = [1, 2, 3, 4, 5]
 y_values = [1, 8, 27, 64, 125]
 
 fig, ax = plt.subplots()
-ax.plot(x_values, y_values, c=y_values, cmap=plt.cm.Blues, linewidth=3)
+ax.plot(x_values, y_values, c='red', linewidth=3)
 
 plt.show()
