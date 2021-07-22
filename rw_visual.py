562c5ec6de7472d2298121614f4d1173796ec05b
Tony-Otis <otieno.anthonyomondi00@gmail.com>
2021-07-22 23:12:12 +0300

    Update rw_visual.py

--- rw_visual.py
+++ rw_visual.py
@@ -11,16 +11,20 @@ while True:
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(14,8), dpi=128)
     #generate lit of numbers equal to the number of points in the walk
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Reds,
-               edgecolors='none', s=15)
+               edgecolors='none', s=1)
 
     # Emphasize the first and last points.
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='blue', edgecolors='none',
     s=100)
+
+    #Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
     plt.show()
 
