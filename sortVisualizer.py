99fdc9ba9454a998a9099058af9543a27a1089e7
Tony-Otis <otieno.anthonyomondi00@gmail.com>
2021-07-24 13:12:37 +0300

    Update sortVisualizer.py

--- sortVisualizer.py
+++ sortVisualizer.py
@@ -6,7 +6,7 @@ from matplotlib.animation import FuncAnimation, writers
 # create random array
 
 x = random.randrange(20, 50)
-array = [i for i in range(1, x - 1)]
+array = [i for i in range(1, x + 1)]
 random.shuffle(array)
 
 
@@ -34,7 +34,7 @@ def insertion_sort(array):
 
 generator = insertion_sort(array)
 
-(fig, ax) = plt.subplots(figsize=(15,9), dpi=128)
+(fig, ax) = plt.subplots()
 ax.set_title('SortVisualizer', fontsize=20)
 plt.xlabel('Bars')
 plt.ylabel('Values')
@@ -43,15 +43,15 @@ set_xlim = (0, x)
 set_ylim = 1.2 * x
 text = ax.text(0.01, 0.95, '', transform=ax.transAxes)
 iteration = [0]
-rects = ax.bar(range(len(array)), array, align='edge')
+rect = ax.bar(range(len(array)), array, align='edge')
 
 
-def animate(array, rects, iteration):
+def animate(array, rect, iteration):
 
     # setting the size of each bar equal
     # to the value of the elements
 
-    for (rect, val) in zip(rects, array):
+    for (rect, val) in zip(rect, array):
         rect.set_height(val)
 
     iteration[0] += 1
@@ -60,15 +60,15 @@ def animate(array, rects, iteration):
 animation = FuncAnimation(
     fig,
     func=animate,
-    fargs=(rects, iteration),
+    fargs=(rect, iteration),
     frames=generator,
     interval=4,
     repeat=False,
     )
 
 #setting up writers object
-writer=writers['pillow']
-writer = writer(fps=30, metadata={'artist': 'Me'}, bitrate=1800)
+# writer=writers['pillow']
+# writer = writer(fps=30, metadata={'artist': 'Me'}, bitrate=1800)
 
-animation.save('Insertion Sort Visualization.gif')
+# animation.save('Insertion Sort Visualization.gif')
 plt.show()
