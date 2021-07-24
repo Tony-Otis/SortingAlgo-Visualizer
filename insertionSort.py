be2f5e289fbc66625d8a181ce2dad8290d8e9e68
Tony-Otis <otieno.anthonyomondi00@gmail.com>
2021-07-24 13:29:35 +0300

    Create insertionSort.py

---
+++ insertionSort.py
@@ -0,0 +1,20 @@
+def insertion_sort(array):
+
+    for i in range(1, len(array)):
+
+        key = array[i]
+
+        j = i - 1
+
+        while j >= 0 and key < array[j]:
+
+            array[j + 1] = array[j]
+            j -= 1
+
+            yield array
+
+        array[j + 1] = key
+
+        # array[i].set_color('r')
+
+        yield array
