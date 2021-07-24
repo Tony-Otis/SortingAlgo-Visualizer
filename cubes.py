cac9657e6fd6174286678f9950d76357964cb4b9
Tony-Otis <otieno.anthonyomondi00@gmail.com>
2021-07-24 13:16:28 +0300

    delete

--- cubes.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 8, 27, 64, 125]
-
-fig, ax = plt.subplots()
-ax.plot(x_values, y_values, c='red', linewidth=3)
-
-plt.show()
