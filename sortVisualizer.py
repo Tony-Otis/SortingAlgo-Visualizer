3933825f4a25d5f575c734516ed2b05f5f0802fb
Tony-Otis <otieno.anthonyomondi00@gmail.com>
2022-07-02 22:04:40 +0300

    implemented bubble sort

--- sortVisualizer.py
+++ sortVisualizer.py
@@ -2,10 +2,12 @@ import random
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, writers
+from bubble_sort import bubbleSort
 
 from insertionSort import insertion_sort
 from heapsort import heapSort
 from selectionsort import selection_sort
+from bubble_sort import bubbleSort
 
 # create random array
 
@@ -15,13 +17,15 @@ random.shuffle(array)
 
 
 generator = int(
-    input('choose algorith, 1:insertion, 2:selection, 3:heap_sort  :'))
+    input('choose algorith, 1:insertion, 2:selection, 3:heap_sort, 4:bubblesort  :'))
 if generator == 1:
     generator = insertion_sort(array)
 elif generator == 2:
     generator = selection_sort(array)
 elif generator == 3:
     generator = heapSort(array)
+elif generator == 4:
+    generator = bubbleSort(array)
 
 (fig, ax) = plt.subplots()
 ax.set_title('Algorithm', fontsize=20)
